fe4efda4cf9b10e6935e081cadf972dde3b5b041
LeshaInc <includeurl@gmail.com>
2024-05-17 01:36:25 +0300

    Move time.rs to beat.rs

--- crates/rdaw-object/src/lib.rs
+++ crates/rdaw-object/src/lib.rs
@@ -1,9 +1,9 @@
+mod beat;
 mod beat_map;
 mod item;
-mod time;
 mod track;
 
+pub use self::beat::{BeatTime, Time};
 pub use self::beat_map::BeatMap;
 pub use self::item::{Item, ItemId};
-pub use self::time::{BeatTime, Time};
 pub use self::track::{Track, TrackId, TrackItem, TrackItemId};
